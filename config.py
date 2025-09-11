3e81479c15742e62f024ea7c714e9d6223cd5acc
dhruvi Sutariya <dhruvi.sutariya@einfochips.com>
2025-09-11 08:51:57 +0530

    changes in ui

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
     # Azure Functions
 DURABLE_STARTER_URL = os.getenv(
         "PARSE_FUNCTION_URL",
-        "https://dev-tmtcopilot-func-bedma3g8buhnczbj.centralindia-01.azurewebsites.net/api/process"
+        "http://localhost:7071/api/process"
     )
 AZURE_DOWNLOAD_FUNCTION_URL = os.getenv(
         "AZURE_DOWNLOAD_FUNCTION_URL",
